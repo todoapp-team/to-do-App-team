e825297ef9a2cb3ba5ef79fe71fc67c0fa685d57
jangap411 <JedidiahANGAP@switchmaven.com>
2021-02-19 15:54:19 +1000

    updates app.js

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 
+//get the variables
 let toDoItems = document.getElementById("to-do-items")
 let toItem = document.getElementById("to-do-item")
 let addToDo = document.getElementById("add-to-do")
 let toDo = document.getElementById("to-do")
 let doneTask = document.getElementById("to-do-li")
 
-//add listener to the button
+//add eventListen to the add button
 addToDo.addEventListener('click',()=>{
+  
   //create li
   let listItem = document.createElement('li')
-  listItem.id="closeItem"
-  console.log('task added...')
+  listItem.id = "closeItem"
   listItem.innerText = toDo.value
+  toDo.value = ""
   toDoItems.appendChild(listItem)
-
-
-  //checkbox
-  let checkbox = document.createElement('input')
-  //assign attribute to checkbox
-  checkbox.type = 'checkbox'
-  checkbox.className = "done-task"
-  listItem.appendChild(checkbox)
-  let completed = document.querySelectorAll(".done-task")
   
-  //add listener for the checkbox item
-    for(let i = 0; i < completed.length;i++){
-      completed[i].addEventListener('click', ()=>{
-        console.log("checkbox clicked")
-        listItem.remove()
-      })
-    }
+  let items = document.querySelectorAll("#closeItem")
+  for (let i = 0; i<items.length;i++){
+    //add double event to each task item
+    items[i].addEventListener('click', ()=>{
+      
+      if (true){
+        console.log(`Click item ${i}`)
+        items[i].className = "cancel"
+        items[i].addEventListener('dblclick', (e)=>{
+          console.log(`item ${i} removed`)
+          items[i].remove(e)
+        })
+      }
+    })
+  }
 })
